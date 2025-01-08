62d508ea09c0d7369dceafe6e8a1b823b652f3f5
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2025-01-08 08:12:10 +0500

    completed the additional task toDoList

--- src/components/ImageCard.styled.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-export const Image = styled.img`
-   width: 100%;
-   border-radius: 10px;
-`;
--- src/components/StyledCardWrapper.styled.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-export const StyledCardWrapper = styled.div`
-   display: flex;
-   justify-content: space-around;
-   align-items: center;
-   min-height: 100vh;
-`;
