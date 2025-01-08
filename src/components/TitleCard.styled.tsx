2857535879b5744f1dc83e3e83386e94dfc06b95
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2025-01-08 13:04:17 +0500

    two tasks completed

---
+++ src/components/TitleCard.styled.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Title = styled.h2`
+   padding: 20px 0px 20px 10px;
+   font-size: 16px;
+   font-weight: 700;
+   //  line-height: 19.36px;
+   text-align: left;
+`;
